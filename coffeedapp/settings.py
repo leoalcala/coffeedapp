603aa6d155f4887752ab40c4781fc21a2c50e903
Leo Alcalá <leoalcala@Leos-MBP.attlocal.net>
2015-04-01 21:28:18 -0700

    Second commit with corrected settings.

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(os.path.abspath(__file__)))
-MAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
@@ -118,3 +119,5 @@ TEMPLATE_DIRS = (
 STATIC_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
     )
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class SplashView(TemplateView):
+    template_name = 'index.html'
